451c531faa39049d753348c8d6f413877ccd73fa
quiemew0 <quiemew@gmail.com>
2024-12-28 18:37:10 +0900

    회원가입

---
+++ src/main/java/JumptoSpringboot/domain/SiteUser.java
@@ -0,0 +1,23 @@
+package JumptoSpringboot.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
