083888958e1b7dd49ef151714f2a6e622f47e121
quiemew0 <quiemew@gmail.com>
2024-12-30 00:30:44 +0900

    검색기능 추가

--- src/main/java/JumptoSpringboot/controller/QuestionController.java
+++ src/main/java/JumptoSpringboot/controller/QuestionController.java
@@ -27,10 +27,12 @@ public class QuestionController {
     private final QuestionService questionService;
     private final UserService userService;
 
+    //value = "kw",defaultValue = "") : kw값이 ㄴ널이 되는것을 방지하기 위해 문자열을 기본값으로 설정
     @GetMapping("/question/list")
-    public String list(Model model,@RequestParam(value="page",defaultValue = "0")int page) {
-        Page<Question> paging = this.questionService.getList(page);
+    public String list(Model model,@RequestParam(value="page",defaultValue = "0")int page,@RequestParam(value = "kw",defaultValue = "")String kw) {
+        Page<Question> paging = this.questionService.getList(page,kw);
         model.addAttribute("paging",paging);
+        model.addAttribute("kw", kw);
 //        List<Question> questionList = this.questionService.getList();
 ///        model.addAttribute("questionList", questionList);
         return "question_list";
--- src/main/java/JumptoSpringboot/service/QuestionService.java
+++ src/main/java/JumptoSpringboot/service/QuestionService.java
@@ -1,13 +1,16 @@
 package JumptoSpringboot.service;
 
+import JumptoSpringboot.domain.Answer;
 import JumptoSpringboot.domain.Question;
 import JumptoSpringboot.domain.SiteUser;
 import JumptoSpringboot.repository.QuestionRepository;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -22,8 +25,12 @@ import java.util.List;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
+    public Page<Question> getList(int page, String kw) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public Question getQuestion(Integer id) {
@@ -63,5 +70,22 @@ public class QuestionService {
     public void vote(Question question, SiteUser siteUser) {
         question.getVoter().add(siteUser);
         this.questionRepository.save(question);
+    }
+    private Specification<Question> search(String kw) {
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"), // 제목  .like : 포함되었는지 확인
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
     }
 }
