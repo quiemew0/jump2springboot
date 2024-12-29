71566f3c1a73c374210a6e04697c621433fd23be
quiemew0 <quiemew@gmail.com>
2024-12-29 22:14:30 +0900

    게시글 삭제하기

--- src/main/java/JumptoSpringboot/controller/QuestionController.java
+++ src/main/java/JumptoSpringboot/controller/QuestionController.java
@@ -95,6 +95,17 @@ public class QuestionController {
         this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/question/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
     // QuestionForm 의 subject, content 속성이 자동으로 바인딩.
 //    @PostMapping("/question/create")
 //    public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
--- src/main/java/JumptoSpringboot/service/QuestionService.java
+++ src/main/java/JumptoSpringboot/service/QuestionService.java
@@ -56,4 +56,7 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         this.questionRepository.save(question);
     }
+    public void delete(Question question) {
+        this.questionRepository.delete(question);
+    }
 }
